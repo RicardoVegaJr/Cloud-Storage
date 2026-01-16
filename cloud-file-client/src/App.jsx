3ab139d4e183e78ffc0918b707c1dc255dab14ce
Ricardo Vega <rvega921@gmail.com>
2026-01-15 22:16:36 -0500

    Add:Login tester place holder. Deactivate:Dashboard

--- cloud-file-client/src/App.jsx
+++ cloud-file-client/src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
-import Dashboard from "./components/Dashboard";
+// import Dashboard from "./components/Dashboard";
 
 
 
 function App() {
 
 const onLogin = () => {
-    navigate("/dashboard");
+    // navigate("/dashboard");
   };
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main onLogin={onLogin} />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </BrowserRouter>
   );
